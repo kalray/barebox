530c4cb11859a6e5cbfee92d14f078277ac674e9
Clement Leger <clement.leger@kalray.eu>
2018-01-18 18:04:10 +0100

    k1c: Cleanup in cpu.c
    Move it to board.c as other architecture are doing.

--- arch/k1c/lib/board.c
+++ arch/k1c/lib/board.c
@@ -3,27 +3,15 @@
  * License.  See the file "COPYING" in the main directory of this archive
  * for more details.
  *
- * Copyright (C) 2017 Kalray Inc.
+ * Copyright (C) 2018 Kalray Inc.
  */
 
-#include <common.h>
 #include <malloc.h>
 #include <memory.h>
 #include <asm-generic/memory_layout.h>
 
-int serial_puts(const char *s)
-{
-	int len = strlen(s);
-	register const char *arg1 asm("r0") = s;
-	register unsigned arg2 asm ("r1") = len;
 
-	asm volatile ("scall 0xffe\n\t;;"
-			: "+r"(arg1), "+r"(arg2)
-			: : "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r11", "memory");
-	return len;
-}
-
-void __noreturn k1c_start_barebox(void)
+void k1c_start_barebox(void)
 {
 	mem_malloc_init((void *)CONFIG_MALLOC_BASE,
 			(void *)(CONFIG_MALLOC_BASE + MALLOC_SIZE - 1));
