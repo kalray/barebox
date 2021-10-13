cf836e5b296fdbe30bfaaa64acd75f96611ff8e4
Clement Leger <cleger@kalray.eu>
2021-10-13 18:23:22 +0200

    remoteproc: Add support for elf64 by using elf loader
    Summary:
    Using the elf loader enables to check the elf buffer before loading it
    and load elf64 by using specific macros.
    
    Depends On D9453
    
    Test Plan: Load an elf file with upcoming kvx remoteproc driver
    
    Reviewers: O73 Barebox Coolidge, lmorhet
    
    Reviewed By: O73 Barebox Coolidge, lmorhet
    
    Subscribers: lmorhet, #linux_coolidge_cc
    
    Differential Revision: https://phab.kalray.eu/D9454

--- drivers/remoteproc/remoteproc_elf_loader.c
+++ drivers/remoteproc/remoteproc_elf_loader.c
@@ -21,67 +21,81 @@
 
 #include "remoteproc_internal.h"
 
-int rproc_elf_load_segments(struct rproc *rproc, const struct firmware *fw)
+static int load_elf_segment(struct rproc *rproc, struct elf_image *elf,
+			    void *phdr)
 {
 	struct device_d *dev = &rproc->dev;
-	struct elf32_hdr *ehdr;
-	struct elf32_phdr *phdr;
-	int i, ret = 0;
-	const u8 *elf_data = fw->data;
+	phys_addr_t da = (phys_addr_t) elf_phdr_p_paddr(elf, phdr);
+	u64 memsz = elf_phdr_p_memsz(elf, phdr);
+	u64 filesz = elf_phdr_p_filesz(elf, phdr);
+	u64 offset = elf_phdr_p_offset(elf, phdr);
+	void *ptr;
 
-	ehdr = (struct elf32_hdr *)elf_data;
-	phdr = (struct elf32_phdr *)(elf_data + ehdr->e_phoff);
+	if (elf_phdr_p_type(elf, phdr) != PT_LOAD)
+		return 0;
+
+	dev_dbg(dev, "phdr: type %d da 0x%llx memsz 0x%llx filesz 0x%llx\n",
+		elf_phdr_p_type(elf, phdr), da, memsz, filesz);
+
+	if (filesz > memsz) {
+		dev_err(dev, "bad phdr filesz 0x%llx memsz 0x%llx\n",
+			filesz, memsz);
+		return -EINVAL;
+	}
+
+	if (offset + filesz > elf->size) {
+		dev_err(dev, "truncated fw: need 0x%llx avail 0x%zx\n",
+			offset + filesz, elf->size);
+		return -EINVAL;
+	}
+
+	/* grab the kernel address for this device address */
+	ptr = rproc_da_to_va(rproc, da, memsz);
+	if (!ptr) {
+		dev_err(dev, "bad phdr da 0x%llx mem 0x%llx\n", da, memsz);
+		return -EINVAL;
+	}
+
+	/* put the segment where the remote processor expects it */
+	if (filesz)
+		memcpy(ptr, elf->hdr_buf + offset, filesz);
+
+	/*
+	 * Zero out remaining memory for this segment.
+	 *
+	 * This isn't strictly required since dma_alloc_coherent already
+	 * did this for us. albeit harmless, we may consider removing
+	 * this.
+	 */
+	if (memsz > filesz)
+		memset(ptr + filesz, 0, memsz - filesz);
+
+	return 0;
+}
+
+int rproc_elf_load_segments(struct rproc *rproc, const struct firmware *fw)
+{
+	void *buf, *phdr;
+	int i, ret = 0;
+	struct elf_image *elf;
+
+	elf = elf_open_binary((void *) fw->data, fw->size);
+	if (!elf)
+		return -EINVAL;
+
+	buf = elf->hdr_buf;
+	phdr = (void *) (buf + elf_hdr_e_phoff(elf, buf));
 
 	/* go through the available ELF segments */
-	for (i = 0; i < ehdr->e_phnum; i++, phdr++) {
-		u32 da = phdr->p_paddr;
-		u32 memsz = phdr->p_memsz;
-		u32 filesz = phdr->p_filesz;
-		u32 offset = phdr->p_offset;
-		void *ptr;
-
-		if (phdr->p_type != PT_LOAD)
-			continue;
-
-		dev_dbg(dev, "phdr: type %d da 0x%x memsz 0x%x filesz 0x%x\n",
-			phdr->p_type, da, memsz, filesz);
-
-		if (filesz > memsz) {
-			dev_err(dev, "bad phdr filesz 0x%x memsz 0x%x\n",
-				filesz, memsz);
-			ret = -EINVAL;
+	for (i = 0; i < elf_hdr_e_phnum(elf, buf); i++) {
+		ret = load_elf_segment(rproc, elf, phdr);
+		if (ret)
 			break;
-		}
 
-		if (offset + filesz > fw->size) {
-			dev_err(dev, "truncated fw: need 0x%x avail 0x%zx\n",
-				offset + filesz, fw->size);
-			ret = -EINVAL;
-			break;
-		}
-
-		/* grab the kernel address for this device address */
-		ptr = rproc_da_to_va(rproc, da, memsz);
-		if (!ptr) {
-			dev_err(dev, "bad phdr da 0x%x mem 0x%x\n", da, memsz);
-			ret = -EINVAL;
-			break;
-		}
-
-		/* put the segment where the remote processor expects it */
-		if (phdr->p_filesz)
-			memcpy(ptr, elf_data + phdr->p_offset, filesz);
-
-		/*
-		 * Zero out remaining memory for this segment.
-		 *
-		 * This isn't strictly required since dma_alloc_coherent already
-		 * did this for us. albeit harmless, we may consider removing
-		 * this.
-		 */
-		if (memsz > filesz)
-			memset(ptr + filesz, 0, memsz - filesz);
+		phdr += elf_size_of_phdr(elf);
 	}
+
+	elf_close(elf);
 
 	return ret;
 }
